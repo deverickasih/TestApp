8fff6b8e880354b4156f7aee7464a273fd3555c4
deverickasih <erick.kasih@gmail.com>
2021-03-08 08:13:42 +0700

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by JAN FREDRICK on 08/03/21.
+//  Copyright © 2021 JFSK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
